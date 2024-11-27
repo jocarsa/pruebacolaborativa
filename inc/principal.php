46218223df23d4365f7b1fbf4e73f398d39ab3f3
AgusMADEV <agustin.morcillo1912@gmail.com>
2024-11-27 20:10:30 +0100

    principal

--- inc/principal.php
+++ inc/principal.php
@@ -5,11 +5,19 @@
 	height:100%;
 	padding-top:125px;
 	}
+	main h1{
+		font-size:30px;
+		color:white;
+		text-align: left;
+		align-items: center;
+	}
 	main p{
 		color: #c2a464;
+		font-size:10px;
 	}
 </style>
 <!-- Pon a continuacion tu contenido -->
  <main>
+	<h1>Título by<br> AgusMADEV</h1>
 	<p>Este es un texto de Agustin</p>
  </main>
